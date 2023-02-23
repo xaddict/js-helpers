555f7608e751f5d8aedcc3b38c239feae3ad7977
xaddict <darklightdusk@gmail.com>
2023-02-23 22:38:36 +0100

    3 files modified, 1 deleted

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,18 @@
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    devSourcemap: true,
+  },
+});
